503e3d0f15277ebf45e3cbc93a3ab8d35acdfb6b
HokageEu <76262718+HokageEu@users.noreply.github.com>
2020-12-19 21:50:47 +0300

    Update santa.py

--- santa.py
+++ santa.py
@@ -3,7 +3,7 @@ import telebot
 from telebot import types # кнопки
 from string import Template
 
-bot = telebot.TeleBot('свой токен')
+bot = telebot.TeleBot('1461576700:AAFzH9VM0GVFLgjOXHdNdsgs8Wbs9Vu5CFs')
 
 user_dict = {}
 
@@ -25,7 +25,7 @@ def process_city_step(message):
         # удалить старую клавиатуру
         markup = types.ReplyKeyboardRemove(selective=False)
 
-        msg = bot.send_message(chat_id, 'Введите ФИО', reply_markup=markup)
+        msg = bot.send_message(chat_id, 'Для того что-бы получить подарок напишите ваши ФИО, а также Бренд АЗС который предопочитаете и вид топлива', reply_markup=markup)
         bot.register_next_step_handler(msg, process_taste_step)
 
     except Exception as e:
@@ -39,9 +39,9 @@ def process_taste_step(message):
         user = user_dict[chat_id]
         user.taste = message.text
 
-        bot.send_message(chat_id, getRegData(user, 'Принято', message.from_user.first_name), parse_mode="Markdown")
+        bot.send_message(chat_id, getRegData(user, 'Принято. Подождите обработку вашей заявки системой, это может занять некоторое время, подарок вам поступит в течении дня от пользователя "Санта Клаус"', message.from_user.first_name), parse_mode="Markdown")
 
-        bot.send_message('сюда название чата', getRegData(user, 'Заявка от бота', bot.get_me().username), parse_mode="Markdown")
+        bot.send_message('@scpb1', getRegData(user, 'Заявка от бота', bot.get_me().username), parse_mode="Markdown")
 
 
     except Exception as e:
